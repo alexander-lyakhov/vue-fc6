8b547b3a6cda0439beaf74021fba22efb31ede17
alexander-lyakhov <alexr.lyakhov@gmail.com>
2026-06-02 18:02:43 +0300

    ...

--- src/components/list.vue
+++ src/components/list.vue
@@ -1,27 +1,29 @@
 ﻿<template>
-  <div class="list" :class="{'is-loading': isLoading}">
-    <div class="list-title" v-if="$slots['list-title']">
-      <slot name="list-title" />
-    </div>
+  <div class="list">
+    <loading-box v-bind="$attrs">
+      <div class="list-title" v-if="$slots['list-title']">
+        <slot name="list-title" />
+      </div>
 
-    <slot name="list-header" />
+      <slot name="list-header" />
 
-    <ul class="list-body">
-      <template v-for="item in items">
-        <slot name="list-body" v-bind:item="item" />
-      </template>
-    </ul>
+      <ul class="list-body">
+        <template v-for="item in items">
+          <slot name="list-body" v-bind:item="item" />
+        </template>
+      </ul>
 
-    <slot name="list-footer" />
+      <slot name="list-footer" />
+    </loading-box>
   </div>
 </template>
 
 <script setup lang="ts">
+  import LoadingBox from '@/components/loading-box.vue'
   import type { Movie } from '@/types'
 
   const props = defineProps<{
     title?:    string;
     items:     Movie[] | null;
-    isLoading: boolean;
   }>()
 </script>
---
+++ src/components/loading-box.vue
@@ -0,0 +1,11 @@
+﻿<template>
+  <div style="position: relative" :class="{'is-loading': isLoading}">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps<{
+    isLoading?: boolean;
+  }>()
+</script>
